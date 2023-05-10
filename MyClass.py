c9f453e2990784d80391144b0139c829f5e24348
Dylan Burg <dylanburg15@gmail.com>
2023-05-10 20:03:17 +0200

    Implement test for soustraction method

--- MyClass.py
+++ MyClass.py
@@ -11,6 +11,10 @@ class SimpleMath:
     def addition(a, b):
         return a + b
     
+    @staticmethod
+    def soustraction(a, b):
+        return a - b
+    
 class TestSimpleMath(unittest.TestCase):
     
     def test_addition_ok(self):
@@ -18,3 +22,10 @@ class TestSimpleMath(unittest.TestCase):
     
     def test_addition_nok(self):
         self.assertNotEqual(SimpleMath.addition(1, 3), 2)
+        
+    def test_soustraction_ok(self):
+        self.assertEqual(SimpleMath.soustraction(2, 2), 0)
+    
+    def test_soustraction_nok(self):
+        self.assertNotEqual(SimpleMath.soustraction(1, 3), 2)
+    
